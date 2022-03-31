70f42924cc1b367d36c022892aebc9f41a60d5b4
Daria-Skvortsova <99176875+Daria-Skvortsova@users.noreply.github.com>
2022-03-31 19:38:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -48,31 +48,41 @@ std::string infx2pstfx(std::string inf) {
   TStack<int, 100> stack;
   for (int i = 0; i < inf.length(); i++) {
     if (priority(inf[i]) == 4) {
-      pref.push_back(inf[i]);
-      pref.push_back(' ');
-    }  else {
-         if (priority(inf[i]) == 0) {
-            stack.push(inf[i]);
-         } else if (priority(inf[i]) > priority(stack.get())) {
+      if (priority(inf[i+1]) == 4) {
+        pref.push_back(inf[i]);
+        while (priority(inf[i + 1]) == 4 && i+1 < inf.length()) {
+          pref.push_back(inf[i+1]);
+          i++;
+        }
+        pref.push_back(' ');
+      }  else {
+           pref.push_back(inf[i]);
+           pref.push_back(' ');
+        }
+      }  else {
+           if (priority(inf[i]) == 0) {
              stack.push(inf[i]);
-           }  else if (stack.isEmpty()) {
+           }  else if (priority(inf[i]) > priority(stack.get())) {
                 stack.push(inf[i]);
-             } else if (priority(inf[i]) == 1) {
-               while (priority(stack.get()) != 0) {
-                 pref.push_back(stack.get());
-                 pref.push_back(' ');
-                 stack.pop();
-               }
-               stack.pop();
-             }  else if (priority(inf[i]) <= priority(stack.get())) {
-                  if (priority(stack.get()) > 1) {
-                    pref.push_back(stack.get());
-                    pref.push_back(' ');
-                    stack.pop();
-                  }
-                  stack.push(inf[i]);
-                }
-       }
+              }  else if (stack.isEmpty()) {
+                   stack.push(inf[i]);
+                 }  else if (priority(inf[i]) == 1) {
+                      while (priority(stack.get()) != 0) {
+                        pref.push_back(stack.get());
+                        pref.push_back(' ');
+                        stack.pop();
+                      }
+                      stack.pop();
+                    }  else if (priority(inf[i]) <= priority(stack.get())) {
+                         while ((priority(stack.get()) > 1) &&
+                          !stack.isEmpty()) {
+                           pref.push_back(stack.get());
+                           pref.push_back(' ');
+                           stack.pop();
+                         }
+                         stack.push(inf[i]);
+                       }
+          }
   }
   while (!stack.isEmpty()) {
     pref.push_back(stack.get());
@@ -88,27 +98,33 @@ int eval(std::string pref) {
   int otvet = 0;
   std::string otv_pref;
   for (int i = 0; i < pref.length(); i++) {
-    if (prior_calc(pref[i]) == 5) {
-      stack_p.push(pref[i] - '0');
-    }  else if ((prior_calc(pref[i]) < 5) && (prior_calc(pref[i]) > 0)) {
-         int val1 = stack_p.get();
-         stack_p.pop();
-         int val2 = stack_p.get();
-         stack_p.pop();
-         if (prior_calc(pref[i]) == 1) {
-            otvet = val2 + val1;
+    std::string proverka = "";
+    while (prior_calc(pref[i]) == 5) {
+      proverka.push_back(pref[i]);
+      i++;
+    }
+    if (proverka != "") {
+      stack_p.push(std::stoi(proverka));
+    }
+    if ((prior_calc(pref[i]) < 5) && (prior_calc(pref[i]) > 0)) {
+      int val1 = stack_p.get();
+      stack_p.pop();
+      int val2 = stack_p.get();
+      stack_p.pop();
+    if (prior_calc(pref[i]) == 1) {
+      otvet = val2 + val1;
+      stack_p.push(otvet);
+    }  else if (prior_calc(pref[i]) == 2) {
+         otvet = val2 - val1;
+         stack_p.push(otvet);
+       }  else if (prior_calc(pref[i]) == 3) {
+            otvet = val2 * val1;
             stack_p.push(otvet);
-         }  else if (prior_calc(pref[i]) == 2) {
-              otvet = val2 - val1;
-              stack_p.push(otvet);
-            }  else if (prior_calc(pref[i]) == 3) {
-                 otvet = val2 * val1;
-                 stack_p.push(otvet);
-               } else if (prior_calc(pref[i]) == 4) {
-                   otvet = val2 / val1;
-                   stack_p.push(otvet);
-                 }
-     }
+          }  else if (prior_calc(pref[i]) == 4) {
+               otvet = val2 / val1;
+               stack_p.push(otvet);
+             }
+    }
   }
   otvet = stack_p.get();
   return otvet;
